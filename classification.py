3536ff244ef2da60a1df8dc3d4e4d60b79cddf31
bs1993 <75962130+bs1993@users.noreply.github.com>
2021-02-14 01:15:13 +0200

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,3 +1,4 @@
+#Libraries import
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ from sklearn.metrics import f1_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import warnings
 warnings.filterwarnings("ignore")
-np.random.seed(123)
+np.random.seed(123) 
 
 from numpy import mean
 from numpy import std
@@ -24,10 +25,10 @@ from numpy.random import randn
 from numpy.random import seed
 from matplotlib import pyplot
 
-#fortosi twn dedomenwn apo to csv
+#Loading data from csv file
 mydata = pd.read_csv('mammography.csv')
 
-# #visualize to plithos twn eggrafwn ana katigoria
+#Visualized the Histogram which presents the aggregate of the data of each class
 import seaborn as sns
 sns.countplot(mydata['result'],label="Count")
 plt.show()
@@ -35,17 +36,17 @@ plt.show()
 
 
 
-# ##kratao ta onomata ton   arxikwn steilon pou tha xrisimopoiisoume sto classification
+#Stored in a list the names of each column will use in classification
 feature_names = ['area', 'gray_level', 'gradient_strength', 'fluctuation', 'contrast','low_order_moment']
-X = mydata[feature_names] #ftiaxnomai ton pinaka pou tha exei tis stiles
-y = mydata['result'] #ta labels ton stilon
+X = mydata[feature_names] #Make the matrix with columns
+y = mydata['result'] #make the label of columns
 
-#sxediazoume to scatter matrix
+#Scatter Matrix Implementation
 pd.plotting.scatter_matrix(X)
 
-#apo tin grammi 49 mexri tin grammi 62
-#ypologizetai to correlation metaksy twn xaraktiristikwn
-#kai apomakrynontai ayta poy exoun correlation panw apo 0.9
+
+#Computation of the correlation beetween features
+#and removed the correlated features
 label_encoder = LabelEncoder()
 X.iloc[:,0] = label_encoder.fit_transform(X.iloc[:,0]).astype('float64')
 
@@ -55,45 +56,52 @@ corr = X.corr()
 columns = np.full((corr.shape[0],), True, dtype=bool)
 for i in range(corr.shape[0]):
     for j in range(i+1, corr.shape[0]):
-        if corr.iloc[i,j] >= 0.9:
+        if corr. iloc[i,j] >= 0.9:
             if columns[j]:
                 columns[j] = False
 selected_columns = X.columns[columns]
 X = X[selected_columns]
 
 
-
+#split train and test set (50-50 the best accuracy)
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,train_size=0.7) #spasimo se train kai test
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.50,train_size=0.50) 
+
+
+#dpp Data Normalization with MinMaxMethod
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)  #kanikopoiisi dedomeon
-X_test = scaler.transform(X_test) #kanikopoiisi dedomeon
+X_train = scaler.fit_transform(X_train)  
+X_test = scaler.transform(X_test)
 
-# #logistic regr
+#Logistic Regression Model
 from sklearn.linear_model import LogisticRegression
 logreg = LogisticRegression()
-logreg.fit(X_train, y_train) #ekpaideusi monteloy
+logreg.fit(X_train, y_train) #Model Train
 
 
+#Confusion Matrix
 matrix = plot_confusion_matrix(logreg, X_test, y_test,
                                  cmap=plt.cm.Blues,
                                  normalize='true')
-plt.title('Confusion matrix for our  Logistic Regression')
+plt.title('Confusion matrix for Logistic Regression')
 plt.show(matrix)
 plt.show()
 
 
-predicted_log  = logreg.predict(X_test);
+predicted_log  = logreg.predict(X_test); #Prediction
 
-desired = y_test.values
+#Remove the index
+desired = y_test.values 
 
 
 
-tp=0
-fp=0
-tn=0
-fn=0
+
+#Manually computation of metrics for LR
+tp=0 #true positive
+fp=0 #false positiove 
+tn=0 #true negative
+fn=0 #false negative
 
 for i in range(len(desired)):
     if desired[i]=="'1'" and predicted_log[i]=="'1'":
@@ -109,15 +117,15 @@ print("True Positive:",tp)
 print("False Positive:",fp)
 print("True Negative:",tn)
 print("False Negative:",fn)
-precision = tp/(tp+fp)
+precision = tp/(tp+fp) #precision metric for LR
 
-recall = tp/(tp+fn)
+recall = tp/(tp+fn) #recall metric for LR
 
-accuracy = (tp+tn)/(tp + tn + fp + fn)
+accuracy = (tp+tn)/(tp + tn + fp + fn) #accuracy metric for LR
 
-f1= 2*(precision*recall)/(precision+recall)
+f1= 2*(precision*recall)/(precision+recall) #f1score metric for LR
 
-specificity = (tn)/(tn+fp)
+specificity = (tn)/(tn+fp) #specificity for LR
 
 
 print("Precision:",precision)
@@ -130,7 +138,8 @@ print("---------------")
 
 
 
-y_pred_proba = logreg.predict_proba(X_test)[::,1]
+#roc curve for Logistic Regression
+y_pred_proba = logreg.predict_proba(X_test)[::,1] #include probalities from model
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba,pos_label="'1'")
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
 plt.plot(fpr,tpr,label="Logistic Regression, auc="+str(auc))
@@ -139,26 +148,27 @@ plt.show()
 
 
 
-# #dec tree
+#Decision Tree Model
 from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier().fit(X_train, y_train)
+clf = DecisionTreeClassifier().fit(X_train, y_train) #Trainning
 
 
-predicted_dc = clf.predict(X_test)
+predicted_dc = clf.predict(X_test) #predection
 
+#Confusion Matrix
 matrix = plot_confusion_matrix(clf, X_test, y_test,
                                  cmap=plt.cm.Blues,
                                  normalize='true')
-plt.title('Confusion matrix for our  Decision Tree')
+plt.title('Confusion matrix for Decision Tree')
 plt.show(matrix)
 plt.show()
 
 
-
-tp=0
-fp=0
-tn=0
-fn=0
+#Manually computation of metrics for DT
+tp=0 #true positive
+fp=0 #false positiove 
+tn=0 #true negative
+fn=0 #false negative
 
 for i in range(len(desired)):
     if desired[i]=="'1'" and predicted_dc[i]=="'1'":
@@ -175,15 +185,15 @@ print("False Positive:",fp)
 print("True Negative:",tn)
 print("False Negative:",fn)
 
-precision = tp/(tp+fp)
+precision = tp/(tp+fp) #precision DT
 
-recall = tp/(tp+fn)
+recall = tp/(tp+fn) #recall DT
 
-accuracy = (tp+tn)/(tp + tn + fp + fn)
+accuracy = (tp+tn)/(tp + tn + fp + fn) #accuracy DT
 
-f1= 2*(precision*recall)/(precision+recall)
+f1= 2*(precision*recall)/(precision+recall) #f1 score DT
 
-specificity = (tn)/(tn+fp)
+specificity = (tn)/(tn+fp) #specificity DT
 
 
 print("Precision:",precision)
@@ -195,7 +205,8 @@ print("Specificity:",specificity)
 print("---------------")
 
 
-y_pred_proba = clf.predict_proba(X_test)[::,1]
+#roc curve for DT
+y_pred_proba = clf.predict_proba(X_test)[::,1] #include probalities from DT model
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba,pos_label="'1'")
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
 plt.plot(fpr,tpr,label="Decision Tree, auc="+str(auc))
@@ -203,26 +214,29 @@ plt.legend(loc=4)
 plt.show()
 
 
-# #K-NN
+#K Nearest Neighbors
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier()
-knn.fit(X_train, y_train)
+knn.fit(X_train, y_train) #Trainning
 
 
-predicted_knn=knn.predict(X_test)
+predicted_knn=knn.predict(X_test) #Prediction
 
+
+#Confusion Matrix
 matrix = plot_confusion_matrix(knn, X_test, y_test,
                                  cmap=plt.cm.Blues,
                                  normalize='true')
-plt.title('Confusion matrix for our  KNN')
+plt.title('Confusion matrix for KNN')
 plt.show(matrix)
 plt.show()
 
 
-tp=0
-fp=0
-tn=0
-fn=0
+#Manually computation of metrics for KNN
+tp=0 #true positive for KNN
+fp=0 #false positiove for KNN
+tn=0 #true negativef or KNN
+fn=0 #false negative for KNN
 
 for i in range(len(desired)):
     if desired[i]=="'1'" and predicted_knn[i]=="'1'":
@@ -240,15 +254,15 @@ print("False Positive:",fp)
 print("True Negative:",tn)
 print("False Negative:",fn)
 
-precision = tp/(tp+fp)
+precision = tp/(tp+fp) #precision KNN
 
-recall = tp/(tp+fn)
+recall = tp/(tp+fn) #Recall KNN
 
-accuracy = (tp+tn)/(tp + tn + fp + fn)
+accuracy = (tp+tn)/(tp + tn + fp + fn) #Accuracy KNN
 
-f1= 2*(precision*recall)/(precision+recall)
+f1= 2*(precision*recall)/(precision+recall) #F1 Score KNN
 
-specificity = (tn)/(tn+fp)
+specificity = (tn)/(tn+fp) #Specificity KNN
 
 
 print("Precision:",precision)
@@ -259,8 +273,8 @@ print("Specificity:",specificity)
 
 print("---------------")
 
-
-y_pred_proba = knn.predict_proba(X_test)[::,1]
+#roc curve for KNN
+y_pred_proba = knn.predict_proba(X_test)[::,1] #include probalities from KNN model
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba,pos_label="'1'")
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
 plt.plot(fpr,tpr,label="KNN, auc="+str(auc))
@@ -268,26 +282,28 @@ plt.legend(loc=4)
 plt.show()
 
 
-# #NV Bayes
+#Naive#include probalities from DT model
 from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
-gnb.fit(X_train, y_train)
+gnb.fit(X_train, y_train) #train
 
-bayes_predicted = gnb.predict(X_test)
+bayes_predicted = gnb.predict(X_test) #prediction
 
+
+#Confusion Matrix
 matrix = plot_confusion_matrix(gnb, X_test, y_test,
                                  cmap=plt.cm.Blues,
                                  normalize='true')
-plt.title('Confusion matrix for our  NV Bayes')
+plt.title('Confusion matrix for NV Bayes')
 plt.show(matrix)
 plt.show()
 
 
-
-tp=0
-fp=0
-tn=0
-fn=0
+#Manually computation of metrics for NVB
+tp=0 #true positive
+fp=0 #false positiove 
+tn=0 #true negative
+fn=0 #false negative
 
 for i in range(len(desired)):
     if desired[i]=="'1'" and bayes_predicted[i]=="'1'":
@@ -304,15 +320,15 @@ print("False Positive:",fp)
 print("True Negative:",tn)
 print("False Negative:",fn)
 
-precision = tp/(tp+fp)
+precision = tp/(tp+fp) #precision for NVB
 
-recall = tp/(tp+fn)
+recall = tp/(tp+fn) #recall for NVB
 
-accuracy = (tp+tn)/(tp + tn + fp + fn)
+accuracy = (tp+tn)/(tp + tn + fp + fn) #accuracy for NVB
 
-f1= 2*(precision*recall)/(precision+recall)
+f1= 2*(precision*recall)/(precision+recall) #F1 Score for NVB
 
-specificity = (tn)/(tn+fp)
+specificity = (tn)/(tn+fp) #Specificity for NVB
 
 
 print("Precision:",precision)
@@ -323,7 +339,7 @@ print("Specificity:",specificity)
 
 print("---------------")
 
-
+#roc curve for NVB
 y_pred_proba = gnb.predict_proba(X_test)[::,1]
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba,pos_label="'1'")
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
@@ -332,14 +348,15 @@ plt.legend(loc=4)
 plt.show()
 
 
-# #SVM
+#Support Vector Machine Model
 from sklearn.svm import SVC
 svm = SVC(probability=True)
-svm.fit(X_train, y_train)
+svm.fit(X_train, y_train) #train
 
-svm_predicted = svm.predict(X_test)
+svm_predicted = svm.predict(X_test) #prediction
 
 
+#Manually computation of metrics for SVM
 matrix = plot_confusion_matrix(svm, X_test, y_test,
                                  cmap=plt.cm.Blues,
                                  normalize='true')
@@ -347,10 +364,11 @@ plt.title('Confusion matrix for SVM')
 plt.show(matrix)
 plt.show()
 
-tp=0
-fp=0
-tn=0
-fn=0
+#Manually computation of metrics for NVB
+tp=0 #true positive
+fp=0 #false positiove 
+tn=0 #true negative
+fn=0 #false negative
 
 for i in range(len(desired)):
     if desired[i]=="'1'" and svm_predicted[i]=="'1'":
@@ -366,15 +384,15 @@ print("True Positive:",tp)
 print("False Positive:",fp)
 print("True Negative:",tn)
 print("False Negative:",fn)
-precision = tp/(tp+fp)
+precision = tp/(tp+fp) #precision for SVM
 
-recall = tp/(tp+fn)
+recall = tp/(tp+fn) #recall for SVM
 
-accuracy = (tp+tn)/(tp + tn + fp + fn)
+accuracy = (tp+tn)/(tp + tn + fp + fn) #accurasy for SVM
 
-f1= 2*(precision*recall)/(precision+recall)
+f1= 2*(precision*recall)/(precision+recall) #f1 score for SVM
 
-specificity = (tn)/(tn+fp)
+specificity = (tn)/(tn+fp) #specificity for SVM
 
 
 print("Precision:",precision)
@@ -388,8 +406,8 @@ print("---------------")
 
 
 
-
-y_pred_proba = svm.predict_proba(X_test)[::,1]
+#roc curve for NVB
+y_pred_proba = svm.predict_proba(X_test)[::,1] #include prob from SVM model
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba,pos_label="'1'")
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
 plt.plot(fpr,tpr,label="SVM, auc="+str(auc))
